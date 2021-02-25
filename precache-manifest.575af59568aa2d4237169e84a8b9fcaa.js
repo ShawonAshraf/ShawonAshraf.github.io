350acce9e8b100ad09547de1c0c6f0ee2606ae8c
Shawon Ashraf <shawon13@live.com>
2021-02-25 23:27:48 +0100

    Updates

---
+++ precache-manifest.575af59568aa2d4237169e84a8b9fcaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6fd7dd5bba4a7eea7d4dcc37acf620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a65b44bb88234b126d1c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "28df48465a5f7313c52b",
+    "url": "/static/js/2.d1258ecf.chunk.js"
+  },
+  {
+    "revision": "fd2f88a8b710726dc05ebe24b0ac79ba",
+    "url": "/static/js/2.d1258ecf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a65b44bb88234b126d1c",
+    "url": "/static/js/main.1d7c8b83.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
