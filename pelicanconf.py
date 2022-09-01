83d94d2d95c7f69e0d3d912eb3869b552dbda14f
Shawon Ashraf <shawon13@live.com>
2022-09-01 13:34:22 +0200

    derp+

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Shawon Ashraf'
 SITENAME = 'Shawon Ashraf'
-SITEURL = 'https://shawonashraf.com'
+SITEURL = ''
 
 PATH = 'content'
 
@@ -16,16 +16,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# links go here
+LINKS = (('Github', 'https://getpelican.com/'),
+         ('Flickr', 'https://www.python.org/'),)
 
 # Social widget
+# socials go here
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
